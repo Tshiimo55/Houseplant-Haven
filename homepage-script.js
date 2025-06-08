b6f923a4e57bfe043f9be9c5710fcf447b691773
Tshiimo55 <153809188+Tshiimo55@users.noreply.github.com>
2025-06-08 11:54:43 +0200

    Add files via upload

---
+++ homepage-script.js
@@ -0,0 +1,14 @@
+// Function to navigate to the Products page
+function goToProducts() {
+  window.location.href = 'products.html';
+}
+
+// Function to navigate to the About page
+function goToAbout() {
+  window.location.href = 'about.html';
+}
+
+// Function to navigate to the Contact page
+function goToContact() {
+  window.location.href = 'contact.html';
+}
